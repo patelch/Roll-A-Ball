2796f7609d1ad5c54c4eed15c6d4ef12d237beba
Charmi Patel <charmip@umd.edu>
2017-05-24 03:50:00 -0400

    completed tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
+	private int count;
 
+	public Text countText;
+	public Text winText;
 	public float speed;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+
+		SetCountText ();
+		winText.text = "";
 	}
 
 	// UpdateUpdate for physics code
@@ -21,5 +29,24 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical);
 
 		rb.AddForce (speed * movement);
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			count += 1;
+			SetCountText ();
+
+			other.gameObject.SetActive (false);
+		}
+	
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 12) {
+			winText.text = "You Win!";
+		}
 	}
 }
